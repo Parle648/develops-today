2454780b8884a32f1a10dc29f1f4c6dacba7852c
Parle648 <sashahoumi@gmail.com>
2024-09-04 22:47:20 +0300

    * Fix result page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 
-import { useEffect, useState } from "react";
-import { options, years } from "./db";
+import { useEffect, useState } from 'react';
+import { options, years } from './db';
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState<boolean>(true);
@@ -9,55 +9,65 @@ export default function Home() {
 
   const changeSelects = (event: any) => {
     setSelects(() => {
-      if (event.target.parentNode.id === "makeId") {
-        return [event.target.value, selects[1]]
+      if (event.target.parentNode.id === 'makeId') {
+        return [event.target.value, selects[1]];
       }
-      if (event.target.parentNode.id === "year") {
-        return [selects[0], event.target.value]
+      if (event.target.parentNode.id === 'year') {
+        return [selects[0], event.target.value];
       }
 
-      return ['', '']
-    })
-  }
+      return ['', ''];
+    });
+  };
 
   useEffect(() => {
     if (selects.some((item: string) => item === '')) {
-      setIsVisible(false)
+      setIsVisible(false);
     } else {
-      setIsVisible(true)
+      setIsVisible(true);
     }
-  }, [selects])
+  }, [selects]);
 
   return (
     <main className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="bg-white p-8 rounded shadow-lg w-96">
-          <h1 className="text-2xl font-bold mb-4 text-center">Select Vehicle Type & Model Year</h1>
+        <h1 className="text-2xl font-bold mb-4 text-center">Select Vehicle Type & Model Year</h1>
 
-          <div className="mb-4">
-              <label className="block text-gray-700">Vehicle Type</label>
-              <select id="makeId" className="w-full mt-2 p-2 border border-gray-300 rounded">
-                <option value="" onClick={changeSelects}>Select Vehicle Type</option>
-                {options.map(option => (
-                <option key={option.id} value={`${option.id}`} onClick={changeSelects}>
-                  {option.name}
-                </option>))}
-              </select>
-          </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Vehicle Type</label>
+          <select id="makeId" className="w-full mt-2 p-2 border border-gray-300 rounded">
+            <option value="" onClick={changeSelects}>
+              Select Vehicle Type
+            </option>
+            {options.map((option) => (
+              <option key={option.id} value={`${option.id}`} onClick={changeSelects}>
+                {option.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-          <div className="mb-6">
-              <label className="block text-gray-700">Model Year</label>
-              <select id="year" className="w-full mt-2 p-2 border border-gray-300 rounded">
-                  <option value="" onClick={changeSelects}>Select Model Year</option>
-                  {years.map((year: number) => (
-                    <option key={year} value={`${year}`} onClick={changeSelects}>
-                    {year}
-                    </option>
-                  ))}
-              </select>
-          </div>
+        <div className="mb-6">
+          <label className="block text-gray-700">Model Year</label>
+          <select id="year" className="w-full mt-2 p-2 border border-gray-300 rounded">
+            <option value="" onClick={changeSelects}>
+              Select Model Year
+            </option>
+            {years.map((year: number) => (
+              <option key={year} value={`${year}`} onClick={changeSelects}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
 
-          <a className={`${isVisible ? '' : 'hidden'} w-full bg-blue-500 text-white py-2 rounded disabled:opacity-50 p-6`} 
-          id="nextButton" href={`./result/[${selects[0]}]/[${selects[1]}]`}>Next</a>
+        <a
+          className={`${isVisible ? '' : 'hidden'} w-full bg-blue-500 text-white py-2 rounded disabled:opacity-50 p-6`}
+          id="nextButton"
+          href={`./result/[${selects[0]}]/[${selects[1]}]`}
+        >
+          Next
+        </a>
       </div>
     </main>
   );
--- src/app/result/[movieId]/[year]/page.tsx
+++ src/app/result/[movieId]/[year]/page.tsx
@@ -1,49 +1,60 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "next/navigation";
 
 async function getVehicles(makeId: string, year: string) {
-    try {
-        const res = await axios.get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`);
-        return res.data;
-    } catch (error) {
-        console.error("Error fetching vehicle data:", error);
-        return null;
-    }
+  try {
+    const res = await axios.get(
+      `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
+    );
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching vehicle data:", error);
+    return null;
+  }
 }
 
 export default function Filters() {
-    const {movieId, year} = useParams();
+  const { movieId, year } = useParams();
 
-    const [vehicles, setVehicles] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
 
-    useEffect(() => {
-        getVehicles(decodeURIComponent(movieId as string).replace(/[\[\]]/g, '') as string, decodeURIComponent(year as string).replace(/[\[\]]/g, '') as string).then(data => {
-            if (data && data.Results) {
-                setVehicles(data.Results);
-            }
-        });
-    }, []);
+  useEffect(() => {
+    getVehicles(
+      decodeURIComponent(movieId as string).replace(/[\[\]]/g, "") as string,
+      decodeURIComponent(year as string).replace(/[\[\]]/g, "") as string
+    ).then((data) => {
+      if (data && data.Results) {
+        setVehicles(data.Results);
+      }
+    });
+  }, []);
 
-    return (
-        <main className="bg-gray-100 flex items-center justify-center min-h-screen">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {vehicles.length > 0 ? (
-                    vehicles.map((vehicle: any) => (
-                        <div key={vehicle.Model_ID} className="max-w-sm bg-white shadow-lg rounded-lg overflow-hidden">
-                            <div className="p-6">
-                                <h2 className="text-xl font-bold text-gray-800">{vehicle.Make_Name}</h2>
-                                <p className="text-gray-600 mt-2">Make ID: <span className="font-semibold">{vehicle.Make_ID}</span></p>
-                                <p className="text-gray-600 mt-1">Vehicle Type: <span className="font-semibold">{vehicle.Vehicle_Type}</span></p>
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <p className="text-gray-600">No vehicles found.</p>
-                )}
-            </div>
-        </main>
-    );
+  return (
+    <main className="bg-gray-100 flex items-center justify-center min-h-screen">
+      <Suspense fallback={"... loading"}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {vehicles.length > 0 ? (
+            vehicles.map((vehicle: any) => (
+              <div key={vehicle.Model_ID} className="max-w-sm bg-white shadow-lg rounded-lg overflow-hidden">
+                <div className="p-6">
+                  <h2 className="text-xl font-bold text-gray-800">{vehicle.Make_Id}</h2>
+                  <p className="text-gray-600 mt-2">
+                    Make Name: <span className="font-semibold">{vehicle.Make_Name}</span>
+                  </p>
+                  <p className="text-gray-600 mt-1">
+                    Model Name: <span className="font-semibold">{vehicle.Model_Name}</span>
+                  </p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-600">No vehicles found.</p>
+          )}
+        </div>
+      </Suspense>
+    </main>
+  );
 }
